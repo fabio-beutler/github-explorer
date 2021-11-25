4c0a1717551303ed9bf58276fdaf6075361c8ea2
Fabio Beutler <dev.fabiobeutler@gmail.com>
2021-11-25 11:26:17 -0400

    Fetch and render user repos

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ import { User } from './components/User'
 
 function App() {
   const [user, setUser] = useState()
+  const [userRepos, setUserRepos] = useState()
 
   return (
     <Container>
-      <SearchBar setUser={setUser} />
-      {user.id && <User user={user} />}
+      <SearchBar setUser={setUser} setUserRepos={setUserRepos} />
+      {user && <User user={user} userRepos={userRepos} />}
       <GlobalStyle />
     </Container>
   )
